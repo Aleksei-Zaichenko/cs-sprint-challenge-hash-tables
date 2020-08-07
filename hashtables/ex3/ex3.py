c96c270f2bb006db3b9c6f99af0b99aaa5e3148b
Aleksei-Zaichenko <aleksei.zaichenko@cix.csi.cuny.edu>
2020-08-07 14:01:02 -0400

    Adds solution for ex5

--- hashtables/ex3/ex3.py
+++ hashtables/ex3/ex3.py
@@ -6,7 +6,10 @@ def intersection(arrays):
     result = []
     dictNumber = {}
 
-    for 
+    for array in arrays:
+        print(array)
+
+    print(dictNumber)
 
     return result
 
@@ -14,8 +17,10 @@ def intersection(arrays):
 if __name__ == "__main__":
     arrays = []
 
-    arrays.append(list(range(1000000, 2000000)) + [1, 2, 3])
-    arrays.append(list(range(2000000, 3000000)) + [1, 2, 3])
-    arrays.append(list(range(3000000, 4000000)) + [1, 2, 3])
+    # arrays.append(list(range(1000000, 2000000)) + [1, 2, 3])
+    # arrays.append(list(range(2000000, 3000000)) + [1, 2, 3])
+    # arrays.append(list(range(3000000, 4000000)) + [1, 2, 3])
+
+    arrays = [[1,2,3],[1,4,5],[1,6,7]]
 
     print(intersection(arrays))
